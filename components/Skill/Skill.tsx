5504e8a0c53b0f83768ff8ba214613461cbbfbb4
oroalej <alexanderjeamoro@gmail.com>
2022-08-02 10:40:32 +0800

    Added Skills, Updated TailwindCSS Version, Allow skill text to be Array, Minor Ui Changes to Skills Item.

--- components/Skill/Skill.tsx
+++ components/Skill/Skill.tsx
@@ -1,25 +1,40 @@
-import React, {FC} from "react"
-import {Icon} from "@icons-pack/react-simple-icons";
+import React, { FC } from "react";
+import { Icon } from "@icons-pack/react-simple-icons";
 
 export interface SkillInterface {
-  icon?: Icon | undefined,
-  text: string
+  icon?: Icon | undefined;
+  text: string | string[];
 }
 
 export const Skill: FC<SkillInterface> = (props: SkillInterface) => {
-  const {text, icon} = props
+  const { text, icon } = props;
   const IconElement: Icon | undefined = icon;
 
   return (
-    <div className="flex flex-row items-center gap-2.5" key={text}>
-      {
-        IconElement ? (
-          <IconElement size={20}/>
+    <div
+      className="flex flex-row items-center gap-2.5"
+      key={Array.isArray(text) ? text[0] : text}
+    >
+      {IconElement ? (
+        <IconElement size={20} />
+      ) : (
+        <div className="w-5 h-5 dark:bg-neutral-200 bg-neutral-700 opacity-90 rounded-sm shrink-0" />
+      )}
+
+      <div className="capitalize">
+        {Array.isArray(text) ? (
+          text.map((item) => (
+            <span
+              className="inline-block select-all whitespace-nowrap [&:not(:last-child):after]:content-['/'] after:mx-2"
+              key={item}
+            >
+              {item}
+            </span>
+          ))
         ) : (
-          <div className="w-5 h-5 dark:bg-neutral-200 bg-neutral-700 opacity-90 rounded-sm"/>
-        )
-      }
-      <span className="select-all capitalize">{text}</span>
+          <span className="select-all">{text}</span>
+        )}
+      </div>
     </div>
-  )
+  );
 };
--- data/skills.ts
+++ data/skills.ts
@@ -1,4 +1,4 @@
-import {SkillInterface} from "@/components/index";
+import { SkillInterface } from "@/components/index";
 import {
   Composer,
   CssThree,
@@ -11,7 +11,8 @@ import {
   Html5,
   Javascript,
   Jira,
-  Laravel, Mariadb,
+  Laravel,
+  Mariadb,
   Mysql,
   Php,
   Phpstorm,
@@ -22,46 +23,51 @@ import {
   Tailwindcss,
   Typescript,
   Vuedotjs,
-  Yarn
+  Yarn,
+  Nuxtdotjs,
 } from "@icons-pack/react-simple-icons";
 
 export const FRONTEND_SKILL: Array<SkillInterface> = [
-  {icon: Html5, text: 'HTML5'},
-  {icon: CssThree, text: 'CSS3 / SASS'},
-  {icon: Tailwindcss, text: 'Tailwind CSS'},
-  {icon: Javascript, text: 'Javascript'},
-  {icon: Javascript, text: 'ECMAScript'},
-  {icon: Typescript, text: 'Typescript'},
-  {icon: Vuedotjs, text: 'VueJS'},
-  // {icon: ReactJs, text: 'ReactJs'},
-  {text: 'Inertia'}
+  { icon: Html5, text: "HTML5" },
+  { icon: CssThree, text: ["CSS3", "SASS"] },
+  { icon: Tailwindcss, text: "Tailwind CSS" },
+  { icon: Javascript, text: "Javascript" },
+  { icon: Javascript, text: "ECMAScript" },
+  { icon: Typescript, text: "Typescript" },
+  { icon: Vuedotjs, text: ["VueJS", "Router"] },
+  { icon: Nuxtdotjs, text: "Nuxt.js" },
+  { text: ["Vuex", "Pinia"] },
+  { icon: ReactJs, text: "ReactJs" },
+  { text: ["Redux-Toolkit"] },
+  { text: ["Next.js"] },
+  { text: "Inertia" },
 ];
 
 export const BACKEND_SKILL: Array<SkillInterface> = [
-  {icon: Laravel, text: 'Laravel'},
-  {icon: Php, text: 'PHP'},
+  { icon: Laravel, text: "Laravel" },
+  { icon: Php, text: "PHP" },
   // {icon: Go, text: 'GoLang'},
   // {icon: Python, text: 'Python'},
-  {text: 'PHPUnit'},
+  { text: "PHPUnit" },
   // {text: 'PestPHP'},
-]
+];
 
 export const DATABASE_SKILL: Array<SkillInterface> = [
-  {icon: Mysql, text: 'Mysql'},
-  {icon: Mariadb, text: 'MariaDB'},
-  {icon: Graphql, text: 'GraphQL'},
+  { icon: Mysql, text: "Mysql" },
+  // { icon: Mariadb, text: "MariaDB" },
+  { icon: Graphql, text: "GraphQL" },
   // {icon: Firebase, text: 'Firebase'},
-  // {text: 'NoSQL'}
-]
+  { text: "NoSQL" },
+];
 
 export const TOOLS_SKILL: Array<SkillInterface> = [
-  {icon: Phpstorm, text: 'Phpstorm'},
-  {icon: Postman, text: 'Postman'},
-  {icon: Figma, text: 'Figma'},
-  {icon: Github, text: 'Github'},
-  {icon: Gitlab, text: 'Gitlab'},
-  {icon: Slack, text: 'Slack'},
-  {icon: Jira, text: 'Jira'},
-  {icon: Yarn, text: 'Yarn'},
-  {icon: Composer, text: 'Composer'},
-]
+  { icon: Phpstorm, text: "Phpstorm" },
+  { icon: Postman, text: "Postman" },
+  { icon: Figma, text: "Figma" },
+  { icon: Github, text: "Github" },
+  { icon: Gitlab, text: "Gitlab" },
+  { icon: Slack, text: "Slack" },
+  { icon: Jira, text: "Jira" },
+  { icon: Yarn, text: "Yarn" },
+  { icon: Composer, text: "Composer" },
+];
