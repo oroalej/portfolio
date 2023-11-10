3883e1c85e7b447228beb7881e56c5217d892005
oroalej <alexanderjeamoro@gmail.com>
2023-11-10 09:56:07 +0800

    Fixed category active state not working properly. Enhancement to useSetParamsRouter: Added remove Param, reset params variable when pathname is changed.

--- src/app/(guest)/quotes/_components/CategoryList.tsx
+++ src/app/(guest)/quotes/_components/CategoryList.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import Link from "next/link";
 import classNames from "classnames";
 import CategoryListLoading from "@/app/(guest)/quotes/_components/Loading/CategoryListLoading";
 import useSetParamsRouter from "@/hooks/useSetParamsRouter";
 import { useGetCategoryList } from "@/features/categories/api";
 
 const CategoryList = () => {
-  const { setParam, push, getParam } = useSetParamsRouter();
+  const { setParam, push, removeParam, getParam } = useSetParamsRouter();
   const { isLoading, data } = useGetCategoryList();
 
   if (isLoading || !data) {
@@ -16,9 +15,7 @@ const CategoryList = () => {
 
   return (
     <div className="overflow-x-auto flex flex-row gap-4 mb-8 pb-4 sm:pb-0 snap-x">
-      <Link
-        href={"/quotes"}
-        shallow
+      <button
         className={classNames(
           "px-3 py-2 cursor-pointer hover:bg-neutral-200 dark:hover:text-neutral-800 rounded-md snap-start ",
           [
@@ -27,9 +24,14 @@ const CategoryList = () => {
               : "text-neutral-800 dark:text-white",
           ]
         )}
+        onClick={() => {
+          setParam("page", 1);
+          removeParam("category_id");
+          push();
+        }}
       >
         All
-      </Link>
+      </button>
 
       {data.map((category) => (
         <button
