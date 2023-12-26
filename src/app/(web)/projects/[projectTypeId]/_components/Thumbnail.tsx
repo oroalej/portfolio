7052e1c7035d5e9ea10dabea675db5de3d5bbf6b
oroalej <alexanderjeamoro@gmail.com>
2023-12-26 20:37:33 +0800

    :bug: Fixed import declaration for ImageLoadingIndicator

--- src/app/(web)/projects/[projectTypeId]/_components/Thumbnail.tsx
+++ src/app/(web)/projects/[projectTypeId]/_components/Thumbnail.tsx
@@ -2,7 +2,6 @@
 
 import classNames from "classnames";
 import SupabaseImage from "@/components/Image/SupabaseImage";
-import { ImageLoadingIndicator } from "@/components";
 import { useLoadable } from "@/hooks";
 import { FC } from "react";
 import { GalleryItem } from "@/context/GalleryContext";
@@ -50,3 +49,17 @@ export const Thumbnail: FC<ThumbnailInterface> = ({
     </div>
   );
 };
+
+export const ImageLoadingIndicator = () => (
+  <div className="absolute inset-0 bg-neutral-200 dark:bg-gray-800 z-40 flex items-center justify-center gap-1.5">
+    <span className="block relative rounded-full h-1.5 w-1.5 bg-gray-500 animate-bounce" />
+    <span
+      className="block relative rounded-full h-1.5 w-1.5 bg-gray-500 animate-bounce"
+      style={{ animationDelay: "250ms" }}
+    />
+    <span
+      className="block relative rounded-full h-1.5 w-1.5 bg-gray-500 animate-bounce"
+      style={{ animationDelay: "400ms" }}
+    />
+  </div>
+);
