191a0a1d70bc53b8e85b3bd22c1fb7ab3690950a
oroalej <alexanderjeamoro@gmail.com>
2024-02-01 23:50:32 +0800

    :bug: Unknown bug. Debugging '?' expected

--- src/app/admin/(modules)/gallery/hooks/useGalleryQueryParams.tsx
+++ src/app/admin/(modules)/gallery/hooks/useGalleryQueryParams.tsx
@@ -1,26 +1,24 @@
 "use client";
 
-import { useQueryState } from "next-usequerystate";
-// import { createSerializer, parseAsString } from "nuqs/parsers";
+// import { useQueryState } from "next-usequerystate";
+import { createSerializer, parseAsString } from "nuqs/parsers";
 
-// export const serializer = createSerializer({
-//   q: parseAsString,
-//   category_id: parseAsString,
-// });
+export const serializer = createSerializer({
+  q: parseAsString,
+  category_id: parseAsString,
+});
 
 export const UseGalleryQueryParams = () => {
-  const [query] = useQueryState("q", {
-    history: "push",
-  });
-
-  const [categoryId] = useQueryState("category_id", {
-    history: "push",
-  });
-
-  // return serializer({
-  //   q: query ?? null,
-  //   category_id: categoryId ?? null,
+  // const [query] = useQueryState("q", {
+  //   history: "push",
+  // });
+  //
+  // const [categoryId] = useQueryState("category_id", {
+  //   history: "push",
   // });
 
-  return "";
+  return serializer({
+    q: null,
+    category_id: null,
+  });
 };
