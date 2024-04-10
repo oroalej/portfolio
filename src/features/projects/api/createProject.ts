af23cd04a63d64887ed6dfb247fe70ebb65a1760
oroalej <alexanderjeamoro@gmail.com>
2024-04-11 01:22:17 +0800

    Feature/Admin - :bug: Fixed compilation error.

--- src/features/projects/api/createProject.ts
+++ src/features/projects/api/createProject.ts
@@ -3,9 +3,21 @@ import { supabase } from "@/utils/supabase";
 import { useMutation } from "@tanstack/react-query";
 import { ProjectFormParams } from "@/app/admin/(modules)/projects/_components/ProjectForm";
 
-export const storeProject = async (formData: ProjectFormParams) => {
+export const storeProject = async ({
+  design_link,
+  repository_link,
+  website_link,
+  screenshots,
+  ...remaining
+}: ProjectFormParams) => {
   const { data: projectId } = await supabase
-    .rpc("store_project", formData)
+    .rpc("store_project", {
+      design_link: design_link ?? undefined,
+      repository_link: repository_link ?? undefined,
+      website_link: website_link ?? undefined,
+      screenshots,
+      ...remaining,
+    })
     .throwOnError();
 
   return projectId;
@@ -14,7 +26,7 @@ export const storeProject = async (formData: ProjectFormParams) => {
 export const useStoreProjectMutation = () => {
   return useMutation({
     mutationFn: async (formData: ProjectFormParams): Promise<string> => {
-      const { data } = await storeProject(formData);
+      const data = await storeProject(formData);
 
       if (data === null) {
         throw new Error("Data not found.");
